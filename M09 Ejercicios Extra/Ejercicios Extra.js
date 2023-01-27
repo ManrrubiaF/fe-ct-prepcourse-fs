e803b8dd7307a2561e1703fdab5661b7b9fd8b08
andsenus <manrrubiaf@gmail.com>
2023-01-26 21:18:39 -0300

    extra home

--- M09 Ejercicios Extra/Ejercicios Extra.js
+++ M09 Ejercicios Extra/Ejercicios Extra.js
@@ -1,11 +1,25 @@
 /*⚠️ NO MODIFIQUES EL NOMBRE DE LAS DECLARACIONES ⚠️*/
 
+const { forEach } = require("../M08 JavaScript Callback/homework");
+
 function deObjetoAarray(objeto) {
    // Recibes un objeto. Tendrás que crear un arreglo de arreglos.
    // Cada elemento del arreglo padre será un nuevo arreglo que contendrá dos elementos.
    // Estos elementos debe ser cada par clave:valor del objeto recibido.
    // [EJEMPLO]: {D: 1, B: 2, C: 3} ---> [['D', 1], ['B', 2], ['C', 3]].
    // Tu código:
+
+  
+   var nuevoarreglo = [];
+   
+   for (var clave in objeto) {
+      nuevoarreglo.push([clave, objeto[clave]]);
+   }
+
+   return nuevoarreglo;
+   
+
+
 }
 
 function numberOfCharacters(string) {
@@ -14,7 +28,22 @@ function numberOfCharacters(string) {
    // Las letras deben estar en orden alfabético.
    // [EJEMPLO]: "adsjfdsfsfjsdjfhacabcsbajda" ---> { a: 5, b: 2, c: 2, d: 4, f: 4, h:1, j: 4, s: 5 }
    // Tu código:
+   var objeto = {};
+   var arrayletra = string.split(""); //==> separa las letras en un array
+   var letrasordenadas = arrayletra.sort();
+   for (var letra of letrasordenadas) {
+      if(!objeto[letra]) {
+         objeto[letra] = 1;
+     
+      } else {
+         objeto[letra] += 1;
+      }
+      
+   }
+   return objeto;
+      
 }
+
 
 function capToFront(string) {
    // Recibes un string con algunas letras en mayúscula y otras en minúscula.
@@ -22,6 +51,20 @@ function capToFront(string) {
    // Retornar el string.
    // [EJEMPLO]: soyHENRY ---> HENRYsoy
    // Tu código:
+   var arraystring = string.split("");
+   var mayusculas = [];
+   var minusculas = [];
+   for(var letra of arraystring){
+      if(letra === letra.toUpperCase()){
+         mayusculas.push(letra);
+      } else {
+         minusculas.push(letra);
+      }
+
+   }
+   return mayusculas.join("") + minusculas.join("");
+
+
 }
 
 function asAmirror(frase) {
@@ -29,18 +72,48 @@ function asAmirror(frase) {
    // La diferencia es que cada palabra estará escrita al inverso.
    // [EJEMPLO]: "The Henry Challenge is close!"  ---> "ehT yrneH egnellahC si !esolc"
    // Tu código:
+   var fraseinvert = ""
+   var nuevafrase = frase.split(" ")
+   for(let i = 0; i < nuevafrase.length ; i++){
+      if(i !== 0)fraseinvert += " " + nuevafrase[i].split("").reverse().join("");
+      else  fraseinvert = nuevafrase[i].split("").reverse().join("");
+      
+   }
+   return fraseinvert;
+
+
 }
 
 function capicua(numero) {
    // Si el número que recibes es capicúa debes retornar el string: "Es capicua".
    // Caso contrario: "No es capicua".
    // Tu código:
+   var numcapicua = ""
+   var newnum = String(numero);
+   for(let i = newnum.length-1 ; i >= 0 ; i --){
+      numcapicua = numcapicua + newnum[i];
+   }
+   if(numcapicua == numero){
+      return "Es capicua"
+   }
+   else return "No es capicua"
+   
 }
 
 function deleteAbc(string) {
    // Tu tarea es eliminar las letras "a", "b" y "c" del string recibido.
    // Retorna el string sin estas letras.
    // Tu código:
+   var newstring = ""
+   for(var i = 0; i < string.length ; i++){
+      if(string[i] !== "a" && string[i] !=="b" && string[i] !== "c"){
+         newstring += string[i] ;
+         
+      }
+      
+   }
+   return newstring;
+   ///return string.split("").filter((e)=> e !== "a" && e !== "b" && e !== "c").join("")
 }
 
 function sortArray(arrayOfStrings) {
@@ -49,6 +122,11 @@ function sortArray(arrayOfStrings) {
    // de la longitud de cada string.
    // [EJEMPLO]: ["You", "are", "beautiful", "looking"]  ---> [“You", "are", "looking", "beautiful"]
    // Tu código:
+   return arrayOfStrings.sort(function(a,b){
+      return a.length - b.length;
+   }) 
+
+
 }
 
 function buscoInterseccion(array1, array2) {
@@ -58,6 +136,21 @@ function buscoInterseccion(array1, array2) {
    // Si no tienen elementos en común, retornar un arreglo vacío.
    // [PISTA]: los arreglos no necesariamente tienen la misma longitud.
    // Tu código:
+   var nuevoarreglo1 = [];
+   for(let i = 0; i < array1.length ; i++){
+      for(let j = 0; j < array2.length ; j ++){
+         if(array1[i] === array2[j]){
+            nuevoarreglo1.push(array1[i]);
+         }
+      }
+
+   }
+   return nuevoarreglo1;
+
+   /*
+   var nuevoarreglo1 = array1.filter(coincide => array2.indexOf(coincide) !== -1)
+   return nuevoarreglo1;
+   */
 }
 
 /*⚠️ NO MODIFIQUES NADA DEBAJO DE ESTO ⚠️*/
